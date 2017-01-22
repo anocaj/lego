d8936466cac4bdf577db82e413b6f1a11c2c2f43
Alban <alban.nocaj@gmail.com>
2017-01-22 16:05:41 +0100

    Motor example

--- legonidas/src/legonidas/Main.java
+++ legonidas/src/legonidas/Main.java
@@ -1,15 +1,31 @@
 package legonidas;
 
+import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
-import lejos.utility.Delay;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 
 public class Main {
+
+	private static EV3MediumRegulatedMotor motorA;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		LCD.drawString("Hello World!", 0, 4);
-		Delay.msDelay(5000);
+//		LCD.drawString("Hello World!", 0, 4);
+//		Delay.msDelay(5000);
+		
+		motorA = new EV3MediumRegulatedMotor(MotorPort.A);
+		LCD.drawString("FORWARD",0,0);
+		motorA.forward();
+		Button.waitForAnyPress();
+		LCD.drawString("BACKWARD",0,0);
+		motorA.backward();
+		Button.waitForAnyPress();
+		motorA.stop();     
+		
+		
 	}
 
 }
